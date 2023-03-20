7ee5d3d4b39b83e3494096b3eb4d14a067b8024b
Kaleb-cpu <kakuj424@gmail.com>
2023-03-20 17:28:21 -0600

    prettier update

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,149 +1,168 @@
-  <svelte:head>
+<svelte:head>
 	<title>blog</title>
 	<meta name="robots" content="noindex nofollow" />
 	<html lang="en" />
 </svelte:head>
-  
-  <main>
-    <section>
-      <h1>BLOG</h1>
-      <div class="rows">
-        <div class="title">
-          <h2>5 Reasons to love Belkin</h2>
-        </div>
-        <div class="content">
-          <a href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds">
-            <div class="card">
-              <img src="./images/blog-img-1.jpeg" alt="brown hand bag"  width="507" height="595">
-              <h4>Brown hand bag</h4> 
-              <p>Excellent craftsmanship and long-lasting materials are key to ensure you can wear it for years to come Renowned for exemplary techniques and Italian artisanship, VONI is a high-in-demand bag brand.</p>
-            </div>
-          </a>
-          <a href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds">
-            <div class="card">
-              <img src="./images/blog-img-2.jpeg" alt="blue hand bag" width="504" height="656">
-              <h4>Blue</h4> 
-              <p>VONI is a high-in-demand bag brand. Made out of intrecciato, the famous leather weave that dates back to the 1960s.VONI is a great choice for anyone who adores subtle design and extra-fine craftsmanship rather than flashy logos.</p>
-            </div>
-          </a>
-          <a href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds">
-            <div class="card">
-              <img src="./images/blog-img-3.jpg" alt="black hand bag" width="513" height="686">
-              <h4>Black hand bag</h4> 
-              <p>The black one is good on the winter, it's very versatile and has the most unique design. You can also wear the black one if you like, on a very rainy spring. These bags are awesome. The blue one also has a unique style to it.</p>
-            </div>
-          </a>
-          <div class="card">
-            <video src="./videos/birklin.mp4" autoplay muted controls title="expensive birkin bags"  width="500" height="300"></video> 
-            <h4>Berkin Bags</h4> 
-            <p>Berkin Bags are One of the most luxirious bags. Birkin bags are handmade from leather and are named after the English-French actress and singer Jane Birkin.</p>
-          </div>
-        </div>
 
-    </section>
-    
-  </main>
+<main>
+	<section>
+		<h1>BLOG</h1>
+		<div class="rows">
+			<div class="title">
+				<h2>5 Reasons to love Belkin</h2>
+			</div>
+			<div class="content">
+				<a
+					href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds"
+				>
+					<div class="card">
+						<img src="./images/blog-img-1.jpeg" alt="brown hand bag" width="507" height="595" />
+						<h4>Brown hand bag</h4>
+						<p>
+							Excellent craftsmanship and long-lasting materials are key to ensure you can wear it
+							for years to come Renowned for exemplary techniques and Italian artisanship, VONI is a
+							high-in-demand bag brand.
+						</p>
+					</div>
+				</a>
+				<a
+					href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds"
+				>
+					<div class="card">
+						<img src="./images/blog-img-2.jpeg" alt="blue hand bag" width="504" height="656" />
+						<h4>Blue</h4>
+						<p>
+							VONI is a high-in-demand bag brand. Made out of intrecciato, the famous leather weave
+							that dates back to the 1960s.VONI is a great choice for anyone who adores subtle
+							design and extra-fine craftsmanship rather than flashy logos.
+						</p>
+					</div>
+				</a>
+				<a
+					href="https://www.michaelkors.ca/handbags/view-all-handbags/_/N-282y?ecid=MKS_TEXT_P_GG_NBPR_CA_EN_HANDBAGS_X_PHRASE_11230790571_109672114229&gclid=CjwKCAiAioifBhAXEiwApzCztnrLse88Prs8qlC7oyIdEhqz0TMP3DMl2-hSC756JV8SZFBKdksg1hoCaEoQAvD_BwE&gclsrc=aw.ds"
+				>
+					<div class="card">
+						<img src="./images/blog-img-3.jpg" alt="black hand bag" width="513" height="686" />
+						<h4>Black hand bag</h4>
+						<p>
+							The black one is good on the winter, it's very versatile and has the most unique
+							design. You can also wear the black one if you like, on a very rainy spring. These
+							bags are awesome. The blue one also has a unique style to it.
+						</p>
+					</div>
+				</a>
+				<div class="card">
+					<video
+						src="./videos/birklin.mp4"
+						autoplay
+						muted
+						controls
+						title="expensive birkin bags"
+						width="500"
+						height="300"
+					/>
+					<h4>Berkin Bags</h4>
+					<p>
+						Berkin Bags are One of the most luxirious bags. Birkin bags are handmade from leather
+						and are named after the English-French actress and singer Jane Birkin.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
 
-  <style>
-    main{
-      flex: auto;
-    }
-/* page title */
-h1{
-  color: #abecf5;
-  font-size: clamp(1rem, 10vw, 4rem); 
-  text-align: center;
-}
+<style>
+	main {
+		flex: auto;
+	}
+	/* page title */
+	h1 {
+		color: #abecf5;
+		font-size: clamp(1rem, 10vw, 4rem);
+		text-align: center;
+	}
 
-h2{
-  color: #90b8f8;
-}
-/* parent */
-section{
-  width: 100%;
-}
+	h2 {
+		color: #90b8f8;
+	}
+	/* parent */
+	section {
+		width: 100%;
+	}
 
-/* rows */
-section .rows{
-  max-width: auto;
-  /* margin: auto; */
-}
+	/* rows */
+	section .rows {
+		max-width: auto;
+		/* margin: auto; */
+	}
 
-/* title in the rows */
-.title{
-  margin: auto;
-  text-align: center;
-}
-.card img{
-  width: 100%;
-  height: initial;
-  object-fit: cover;
-  border-radius: 10px;
-  
-}
+	/* title in the rows */
+	.title {
+		margin: auto;
+		text-align: center;
+	}
+	.card img {
+		width: 100%;
+		height: initial;
+		object-fit: cover;
+		border-radius: 10px;
+	}
 
+	/* card parent */
+	.content {
+		display: grid;
+		grid-auto-flow: column;
+		grid-auto-columns: 2fr 2fr 2fr 4fr;
+	}
 
-/* card parent */
-.content{
-  display: grid;
-  grid-auto-flow: column;
-  grid-auto-columns: 2fr 2fr 2fr 4fr;
-  
-}
+	/* card holder */
+	.card {
+		margin: 2rem 1rem;
+	}
 
-/* card holder */
-.card{
+	/* border box only on images */
+	.card:nth-child(-n + 3) {
+		border: solid 5px #26282b;
+		border-radius: 10px;
+	}
+	/* inside card title 2 */
+	.title h2 {
+		font-size: clamp(1.5rem, 2.5vw, 4rem);
+		font-family: 'Lato', Tahoma, Geneva, Verdana, sans-serif;
+	}
 
-  margin: 2rem 1rem;
-}
+	/* inside card title */
+	.content h4 {
+		padding: 1rem;
+		font-size: clamp(1rem, 2.5vw, 1.5rem);
+		color: #90b8f8;
+	}
 
-/* border box only on images */
-.card:nth-child(-n+3){
-  border: solid 5px #26282b;
-  border-radius: 10px;
+	/* inside card paragraph */
+	.content p {
+		font-size: clamp(1rem, 2vw, 1.2rem);
+		color: #d6d0b8;
+		line-height: 1.5;
+		padding: 1em;
+	}
 
-}
-/* inside card title 2 */
-.title h2{
-  font-size: clamp(1.5rem, 2.5vw, 4rem); 
-  font-family: 'Lato', Tahoma, Geneva, Verdana, sans-serif;
-}
+	/* first three children of card element */
+	.card:nth-child(-n + 3):hover {
+		opacity: 0.5;
+		cursor: pointer;
+	}
 
+	/* hover on image cards only */
+	.card video {
+		width: 100%;
+		border: none;
+	}
 
-/* inside card title */
-.content h4 {
-    padding: 1rem;
-    font-size: clamp(1rem, 2.5vw, 1.5rem); 
-    color: #90b8f8;
-}
-
-
-  /* inside card paragraph */
-.content p{
-  font-size: clamp(1rem, 2vw, 1.2rem); 
-    color: #d6d0b8;
-    line-height: 1.5;
-    padding: 1em;
-} 
-
-/* first three children of card element */
-.card:nth-child(-n+3):hover{
-   opacity: 0.5;
-  cursor: pointer;
-
-}
-
-/* hover on image cards only */
-.card video{
-  width: 100%;
-  border: none;
-}
-
-@media only screen and (max-width: 60em){
-  
-  .content{
-    display: flex;
-    flex-direction: column;
-  }
-}
-  </style>
+	@media only screen and (max-width: 60em) {
+		.content {
+			display: flex;
+			flex-direction: column;
+		}
+	}
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,126 +1,114 @@
-  <svelte:head>
+<svelte:head>
 	<title>contact</title>
 	<meta name="robots" content="noindex nofollow" />
 	<html lang="en" />
 </svelte:head>
-  <main>
-    <h1>Contact US</h1>
-    <div class="contact">
-      <h3>Online Customer Service</h3>
-      <p>Drop us a line through the form below, and we will get back to you ASAP</p>
-      <form
-  action="https://formspree.io/f/xqkodero"
-  method="POST"
->
+<main>
+	<h1>Contact US</h1>
+	<div class="contact">
+		<h3>Online Customer Service</h3>
+		<p>Drop us a line through the form below, and we will get back to you ASAP</p>
+		<form action="https://formspree.io/f/xqkodero" method="POST">
+			<label for="fullname">Full Name</label>
+			<input type="name" name="name" />
 
-  
+			<label for="email">Email Address</label>
+			<input type="email" name="email" />
 
-   <label for="fullname">Full Name</label>
-    <input type="name" name="name">
+			<label for="message">Message</label>
+			<input type="text" name="message" />
 
-    <label for="email">Email Address</label>
-    <input type="email" name="email">
-  
-    <label for="message">Message</label>
-    <input type="text" name="message">                                                         
-                                                        
-    <button type="submit">Send</button>
-                                                          
-</form>
-    </div>
+			<button type="submit">Send</button>
+		</form>
+	</div>
+</main>
 
-  </main> 
-  <style>
-    /* main class element */
-.contact{
-  height: 100vh;
-}
+<style>
+	/* main class element */
+	.contact {
+		height: 100vh;
+	}
 
-/* main */
-main{
-  background: #90b8f8;
-  flex: auto;
-}
+	/* main */
+	main {
+		background: #90b8f8;
+		flex: auto;
+	}
 
-/* texts */
-h1, h3, p{
-  text-align: center; 
-  color: #26282b;
-}
+	/* texts */
+	h1,
+	h3,
+	p {
+		text-align: center;
+		color: #26282b;
+	}
 
+	/* contact form */
+	form {
+		width: 25ch;
+		background: #353941;
+		border-radius: 17px;
+		padding: 20px;
+		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 
- 
-/* contact form */
-  form{
-  
-  
-  width: 25ch;
-  background:#353941;
-  border-radius: 17px;
-  padding: 20px;
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+		margin-left: 2em;
+		font-size: clamp(1.5rem, 2vw, 4rem);
+	}
 
-  margin-left: 2em;
-  font-size: clamp(1.5rem, 2vw, 4rem); 
-  
-}
+	/* form label class */
 
-/* form label class */
+	label {
+		display: block;
+		text-align: start;
+		color: #f2f2f2;
+		font-weight: 600;
+		font-size: clamp(1rem, 1.5vw, 2rem);
+		cursor: pointer;
+		padding-bottom: 0.5em;
+		padding-top: 0.3em;
+	}
 
-label {
-  display: block;
-  text-align: start;
-  color: #f2f2f2;
-  font-weight: 600;
-  font-size: clamp(1rem, 1.5vw, 2rem); 
-  cursor: pointer;
-  padding-bottom: 0.5em;
-  padding-top: 0.3em;
-}
+	/* where people would write */
+	input {
+		border: 3px solid #5f85db;
+		border-radius: 5px;
+		padding: 0.6em;
+		font-size: clamp(1rem, 1.5vw, 2rem);
+	}
+	input[type='text']:focus,
+	input[type='email']:focus {
+		border: 3px solid white;
+	}
 
+	/* submit button */
+	button {
+		border: none;
+		height: 50px;
+		width: 100%;
+		font-size: clamp(1.5rem, 2vw, 4rem);
+		background-color: rgb(17, 59, 71);
+		border: none;
+		border-radius: 5px;
+		color: hsl(170, 30%, 80%);
+		transition: transform 0.5s;
+		margin-top: 0.6em;
+	}
 
-/* where people would write */
-input {
-  border: 3px solid #5f85db;
-  border-radius: 5px;
-  padding: 0.6em;
-  font-size: clamp(1rem, 1.5vw, 2rem); 
-}
-input[type="text"]:focus, input[type="email"]:focus {
-border: 3px solid white;
-}
+	/* button hover */
+	button:hover {
+		background-color: rgb(21, 25, 26);
+		color: rgba(235, 235, 235, 0.733);
+		cursor: pointer;
+		transform: scale(1.03);
+	}
 
-/* submit button */
-button{
-  border: none;
-  height: 50px;
-   width: 100%;
-  font-size: clamp(1.5rem, 2vw, 4rem); 
-  background-color: rgb(17, 59, 71);
-  border: none;
-  border-radius: 5px;
-  color: hsl(170, 30%, 80%);
-  transition: transform .5s;
-  margin-top: 0.6em;
-}
-
-/* button hover */
-button:hover{
-  background-color: rgb(21, 25, 26);
-  color: rgba(235, 235, 235, 0.733);
-  cursor: pointer;
-  transform: scale(1.03);
-}
-
-/* responsive on mobile */
-@media only screen and (max-width: 60em) {
-  form{
-    display: grid;
-    grid-template-rows: auto;
-    padding-left: 1rem;
-    width: 15ch;
-  }
-
-}
-
-  </style>
+	/* responsive on mobile */
+	@media only screen and (max-width: 60em) {
+		form {
+			display: grid;
+			grid-template-rows: auto;
+			padding-left: 1rem;
+			width: 15ch;
+		}
+	}
+</style>
