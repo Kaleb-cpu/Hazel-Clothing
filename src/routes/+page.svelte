436cd01d306a6a9a066034f63ee8be7303e99ff6
Kaleb-cpu <kakuj424@gmail.com>
2023-03-20 22:30:52 -0600

    home page h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <main>
 
-  <h1>Shop now</h1>
+  <h1>Shop Now</h1>
 
 	<video id="background-video" autoplay loop controls muted poster>
   <source src="./videos/How to Break into the Fashion Industry with Alexa Chung S1, E1 Future of Fashion British Vogue.mp4" type="video/mp4">
@@ -31,13 +31,25 @@
 }
 
 h1 {
+  display: flex;
+  justify-content: flex-end;
   color: white;
   font-family: Trebuchet MS;
   font-weight: bold;
   text-align: center;
-  font-size: clamp(1rem, 10vw, 5rem); ;
-  margin-top: 30vh;
-  margin-left: 90vh;
+  font-size: clamp(1rem, 10vw, 5rem);
+  margin-top: 55vh;
+  float: right;
+  margin-right: 1rem;
+  width: max-content;
+  border: 7px solid #90b8f8;
+  padding: 2.2rem 1rem;
+  border-radius: 10px;
+}
+
+h1:hover{
+  color: #353941;
+  cursor: pointer;
 }
 
 
